7edff449bac52ecfac8e3666659e653d4c29a18e
Jacob Haller-Roby <jacob@fusebit.io>
2021-10-05 22:25:11 -0700

    old const

--- src/constants.ts
+++ src/constants.ts
@@ -10,13 +10,8 @@ export const DataKeyMap: DataKeyMap = {
   tasks: 'tasks',
 };
 
-// Replace each value with a valid integration id
-export const IntegrationTypeIdMap = {
-  [IntegrationTypeEnum.slack]: 'slack-integration',
-  [IntegrationTypeEnum.hubspot]: 'hubspot-integration',
-};
 export const AssertsIsKeyOf: <T extends string>(key: string, map: Record<T, any>) => asserts key is T = (input) => {
-  if (!Object.keys(IntegrationTypeIdMap).includes(input)) {
+  if (!Object.keys(map).includes(input)) {
     throw `Invalid Integration Type ${input}`;
   }
 };
