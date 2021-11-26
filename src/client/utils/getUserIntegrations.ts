b0327c98bcb44f28dc41f19409abbc6500a3a0b6
Federico Peralta <fperalta@litebox.ai>
2021-11-26 16:21:32 -0300

    read envs from webpack

--- src/client/utils/getUserIntegrations.ts
+++ src/client/utils/getUserIntegrations.ts
@@ -1,16 +1,18 @@
 import jwt_decode from 'jwt-decode';
 
+declare var env: Config
 
 export const getUserIntegrations = () => {
+    let config = {} as Config
     const token = localStorage.getItem('configuration')
 
-    if (!token) {
-        return []
+    if (token) {
+        config = jwt_decode(token)
+    } else {
+        config = env
     }
 
-    const config = jwt_decode(token) as Config
-
-    return Object.keys(config).filter(key => key.endsWith('INTEGRATION_ID')).map(i => {
+    return Object.keys(config).filter(key => key.endsWith('INTEGRATION_ID') && !!config[key as keyof Config]).map(i => {
         return {
             feedId: i.split('_')[0],
             id: config[i as keyof Config]
