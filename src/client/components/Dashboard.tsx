d12d584091ab518f5bb3064607bfdc2dc5f8eb30
Shehzad Akbar <akbar.shehzad@gmail.com>
2022-04-04 23:59:49 -0400

    Paperbox Guide Updates

--- src/client/components/Dashboard.tsx
+++ src/client/components/Dashboard.tsx
@@ -148,10 +148,11 @@ export default (props: { userData: UserData; appToTest: Feed; isInstalled: boole
                 Fusebit automatically checks if the specific user (or user) has installed the integration in their
                 account. You can use this information to enable / disable different actions in the system.
               </Typography>
+              <br></br>
               <Typography>
                 {`In this example, the "Add New ${getItemName(
                   props.appToTest
-                )}" Button, if installed, will execute your Fusebit Integration! 
+                )}" Button, if installed, will execute your Fusebit Integration.  
                 Look at the code to see how it works by looking through the GitHub Repo`}
               </Typography>
             </>
